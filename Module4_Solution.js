93b8d0a845b9ef719729ddd0049c050962b792f1
aitbenmoumen <114586538+aitbenmoumen@users.noreply.github.com>
2023-04-08 19:23:22 +0200

    Update Module4_Solution.js

--- Module4_Solution.js
+++ Module4_Solution.js
@@ -1,4 +1,4 @@
-var names = ["Adam", "jane", "James", "Ahmed", "Jill", "Doha", "Albert"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0);
